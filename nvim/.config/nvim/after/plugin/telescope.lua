8783953cf9cba782c44561cd18bbefa2ee4c43ea
JosephGonzalez03 <josephdg3@gmail.com>
2023-01-11 21:03:59 -0600

    feat: Add telescope previewer colors

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -30,6 +30,12 @@ require('telescope').setup({
     },
 })
 
+-- Set previewer colors
+vim.api.nvim_set_hl(0, "FloatBorder", {bg="#3B4252", fg="#5E81AC"})
+vim.api.nvim_set_hl(0, "NormalFloat", {bg="#3B4252"})
+vim.api.nvim_set_hl(0, "TelescopeNormal", {bg="#3B4252"})
+vim.api.nvim_set_hl(0, "TelescopeBorder", {bg="#3B4252"})
+
 local Telescope = setmetatable({}, {
     __index = function(_, k)
         if vim.bo.filetype == 'NvimTree' then
